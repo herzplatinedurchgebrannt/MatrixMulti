c266f0c35ba35962c7a9f4d953944a5758593af3
lx <alexandermathieu@googlemail.com>
2022-01-05 19:37:00 +0100

    calcultion KR210 not working

--- MatrixMulti/Program.cs
+++ MatrixMulti/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 
-namespace MatrixMulti 
+namespace MatrixMulti
 {
     public static class Matrix
     {
@@ -57,7 +57,7 @@ namespace MatrixMulti
             return result;
         }
 
-        public static double calculateScalarProduct (double [,] vectorA, double [,] vectorB)
+        public static double calculateScalarProduct(double[,] vectorA, double[,] vectorB)
         {
             return 0;
         }
@@ -72,7 +72,7 @@ namespace MatrixMulti
             double[,] sub3x3Matrix3 = new double[3, 3];
             double[,] sub3x3Matrix4 = new double[3, 3];
 
-            double[,,] test =new double[4, 3, 3];
+            double[,,] test = new double[4, 3, 3];
 
             double[,] S = new double[3, 3];
 
@@ -84,23 +84,23 @@ namespace MatrixMulti
                 int l = 0;
                 for (int c = 0; c < AColumns; c++)
                 {
-                    if (c == 0) {}
-                    else { sub3x3Matrix1[r-1, h] = A[r, c]; test[0, r - 1, h] = A[r, c]; h += 1; }
+                    if (c == 0) { }
+                    else { sub3x3Matrix1[r - 1, h] = A[r, c]; test[0, r - 1, h] = A[r, c]; h += 1; }
 
-                    if (c == 1) {}
-                    else { sub3x3Matrix2[r-1, j] = A[r, c]; test[1, r - 1, j] = A[r, c]; j += 1; }
+                    if (c == 1) { }
+                    else { sub3x3Matrix2[r - 1, j] = A[r, c]; test[1, r - 1, j] = A[r, c]; j += 1; }
 
-                    if (c == 2) {}
-                    else { sub3x3Matrix3[r-1, k] = A[r, c]; test[2, r - 1, k] = A[r, c]; k += 1; }
+                    if (c == 2) { }
+                    else { sub3x3Matrix3[r - 1, k] = A[r, c]; test[2, r - 1, k] = A[r, c]; k += 1; }
 
-                    if (c == 3) {}
-                    else { sub3x3Matrix4[r-1, l] = A[r, c]; test[3, r - 1, l] = A[r, c]; l += 1; }
+                    if (c == 3) { }
+                    else { sub3x3Matrix4[r - 1, l] = A[r, c]; test[3, r - 1, l] = A[r, c]; l += 1; }
                 }
             }
 
             // https://matheguru.com/lineare-algebra/determinante.html
 
-            double resultSubMatrix =   S[0, 0] * S[1, 1] * S[2, 2]
+            double resultSubMatrix = S[0, 0] * S[1, 1] * S[2, 2]
                                      + S[0, 1] * S[1, 2] * S[2, 0]
                                      + S[0, 2] * S[1, 0] * S[2, 1]
                                      - S[0, 2] * S[1, 1] * S[2, 0]
@@ -130,7 +130,7 @@ namespace MatrixMulti
             double[,] vectorA = new double[3, 1];
             double[,] vectorP = new double[3, 1];
 
-            for (int r = 0; r < ARows-1; r++)
+            for (int r = 0; r < ARows - 1; r++)
             {
                 vectorN[r, 0] = A[r, 0];
                 vectorO[r, 0] = A[r, 1];
@@ -143,16 +143,16 @@ namespace MatrixMulti
             double oSkalar = 0;
             double aSkalar = 0;
 
-            for (int r = 0; r < ARows-1; r++)
+            for (int r = 0; r < ARows - 1; r++)
             {
                 nSkalar += vectorN[r, 0] * vectorP[r, 0];
                 oSkalar += vectorO[r, 0] * vectorP[r, 0];
                 aSkalar += vectorA[r, 0] * vectorP[r, 0];
             }
 
-            for (int r = 0; r < ARows-1; r++)
+            for (int r = 0; r < ARows - 1; r++)
             {
-                for (int c = 0; c < AColumns-1; c++)
+                for (int c = 0; c < AColumns - 1; c++)
                 {
                     result[c, r] = A[r, c];
                 }
@@ -167,7 +167,7 @@ namespace MatrixMulti
         }
 
 
-        public static double[,] getTranslationMatrix (double x = 0, double y = 0, double z = 0)
+        public static double[,] getTranslationMatrix(double x = 0, double y = 0, double z = 0)
         {
             // Verschiebung vom Punkt im Raum
             double[,] translationMatrix = { { 1, 0, 0, x },
@@ -181,8 +181,8 @@ namespace MatrixMulti
         public static double[,] getRotationMatrix(RotationAxis rotationaxis, double angle)
         {
             // Drehung vom Achse
-            double cosO = Math.Round(Math.Cos(angle * Math.PI / 180),9);
-            double sinO = Math.Round(Math.Sin(angle * Math.PI / 180),9);
+            double cosO = Math.Round(Math.Cos(angle * Math.PI / 180), 9);
+            double sinO = Math.Round(Math.Sin(angle * Math.PI / 180), 9);
 
             double[,] rotationMatrix = new double[4, 4];
 
@@ -224,13 +224,13 @@ namespace MatrixMulti
         ///<summary>
         ///Rotates matrix intrinsic 
         ///</summary>
-        public static double[,] rotateMatrixIntrinsic (double A, double B, double C, RotationOrder rotationorder = RotationOrder.ZYX)
+        public static double[,] rotateMatrixIntrinsic(double A, double B, double C, RotationOrder rotationorder = RotationOrder.ZYX)
         {
             // A Rotation um Z
             // B Rotation um Y'
             // C Rotation um X''
 
-            double[,] result = new double[4,4];
+            double[,] result = new double[4, 4];
 
             double[,] RotationA = Matrix.getRotationMatrix(RotationAxis.Z, A);
             double[,] RotationB = Matrix.getRotationMatrix(RotationAxis.Y, B);
@@ -255,14 +255,14 @@ namespace MatrixMulti
             return result;
         }
 
-        public static double[,] axisToAxis( double [,] P, double theta, double d, double a, double alpha)
+        public static double[,] axisToAxis(double[,] P, double theta, double d, double a, double alpha)
         {
             double[,] result = new double[4, 4];
 
             double[,] rotTheta = Matrix.getRotationMatrix(RotationAxis.Z, theta);
             double[,] transD = Matrix.getTranslationMatrix(0, 0, d);
             double[,] transA = Matrix.getTranslationMatrix(a, 0, 0);
-            double[,] rotAlpha  = Matrix.getRotationMatrix(RotationAxis.X, alpha);
+            double[,] rotAlpha = Matrix.getRotationMatrix(RotationAxis.X, alpha);
 
             result = Matrix.multiplyMatrix(P, rotTheta);
             result = Matrix.multiplyMatrix(result, transD);
@@ -283,7 +283,7 @@ namespace MatrixMulti
             {
                 for (int c = 0; c < acolumns; c++)
                 {
-                    Console.Write( A[r, c] + " " );
+                    Console.Write(A[r, c] + " ");
                 }
                 Console.WriteLine();
             }
@@ -302,16 +302,16 @@ namespace MatrixMulti
         public static void Main(string[] args)
         {
 
-            double[,] A = { { 1, 2, 3 ,3 }, 
-                            { 0, 5, 6, 2 }, 
-                            { 1, 2, 0, 2 } , 
+            double[,] A = { { 1, 2, 3 ,3 },
+                            { 0, 5, 6, 2 },
+                            { 1, 2, 0, 2 } ,
                             { 1, 4, 0, 2 } };
 
             Matrix.checkSingularity(A);
 
-            double[,] B = { { 1, 1, 0, 1 }, 
-                            { 0, 2, 6, 1 }, 
-                            { 1, 3, 4, 1 }, 
+            double[,] B = { { 1, 1, 0, 1 },
+                            { 0, 2, 6, 1 },
+                            { 1, 3, 4, 1 },
                             { 0, 2, 1, 3 } };
 
             double[,] E = { { 1, 2, 3 ,3 },
@@ -323,9 +323,9 @@ namespace MatrixMulti
                             { 1, 3, 4 },
                             { 0, 2, 1 } };
 
-            double[,] vektorTransposed = new double[1,4] {{ 1, 1, 0, 1 }};
+            double[,] vektorTransposed = new double[1, 4] { { 1, 1, 0, 1 } };
 
-            double[,] v = new double [4, 1] {{ 1 },
+            double[,] v = new double[4, 1] {{ 1 },
                                              { 1 },
                                              { 0 },
                                              { 1 }};
@@ -343,7 +343,7 @@ namespace MatrixMulti
             //1 9 14 9
             //1 13 26 11
 
-            double[,] p = new double [4,1] {{ 130 },
+            double[,] p = new double[4, 1] {{ 130 },
                                             { -55 },
                                             { 120 },
                                             { 1 }};
@@ -353,6 +353,7 @@ namespace MatrixMulti
                             { 0, 1, 0, 7 },
                             { 0, 0, 1, -4 } ,
                             { 0, 0, 0, 1 } };
+
 
 
             // EXAMPLES
@@ -367,8 +368,6 @@ namespace MatrixMulti
 
             //Matrix.print(c);
 
-            //Console.WriteLine("_----_");
-
             double[,] g = Matrix.rotateMatrixIntrinsic(65, -40, 30, Matrix.RotationOrder.ZYX);
 
             //Matrix.print(g);
@@ -377,12 +376,12 @@ namespace MatrixMulti
 
             //Matrix.print(g);
 
-            //Console.WriteLine("_----_");
+
 
             g = Matrix.multiplyMatrix(E, F);
 
             //Matrix.print(g);
-
+            /*
             double[,] calc = { { 1, 0, 0 ,0 },
                                { 0, 1, 0, 0 },
                                { 0, 0, 1, 0 },
@@ -433,7 +432,111 @@ namespace MatrixMulti
 
             double joint1 = Math.Atan2(calc2[1, 3], calc2[0, 3]) * 180 / Math.PI;
 
-            Console.Write(joint1);
+            Console.Write(joint1);*/
+
+            // start position/ orientation
+            double[,] calc3 = { { 1, 0, 0 ,0 },
+                                { 0, 1, 0, 0 },
+                                { 0, 0, 1, 0 },
+                                { 0, 0, 0, 1 } };
+
+
+            // robot mada
+            // PUMA 560 
+            //double[] d = { 671.83, 139.70, 0, 431.80, 0, 56.50 };
+            //double[] r = { 0, 431.80, -20.32, 0, 0, 0 };
+            //double[] alpha = { -90, 0, 90, -90, 90, 0 };
+
+
+            //
+            // KR 210
+            double[] d = { 675, 0,    0,  1200, 0, 240 };
+            double[] r = { 350, 1150, 41, 0,    0, 0 };
+            double[] alpha = { -90, -90, 90, -90, 90, 0 };
+
+
+            // current joint angles
+            //double[] theta = { 45, 45, 45, 45, 45, 45 };
+            //double[] theta = { 45, 45, 45, 45, 45, 45 };
+            double[] theta = { 45, -45, 45, 45, 45, 45 };
+            //double[] theta = { 0, 0, -90, 0, 30, 0 };
+
+            // forward kinematics
+            calc3 = Matrix.axisToAxis(calc3, theta[0], d[0], r[0], alpha[0]);
+            calc3 = Matrix.axisToAxis(calc3, theta[1], d[1], r[1], alpha[1]);
+            calc3 = Matrix.axisToAxis(calc3, theta[2], d[2], r[2], alpha[2]);
+            calc3 = Matrix.axisToAxis(calc3, theta[3], d[3], r[3], alpha[3]);
+            calc3 = Matrix.axisToAxis(calc3, theta[4], d[4], r[4], alpha[4]);
+            calc3 = Matrix.axisToAxis(calc3, theta[5], d[5], r[5], alpha[5]);
+
+            Matrix.print(calc3);
+
+            double[] joints = new double[6];
+
+            double[] P5 = { calc3[0, 3] - d[5] * calc3[0, 2], calc3[1, 3] - d[5] * calc3[1, 2], calc3[2, 3] - d[5] * calc3[2, 2] };
+
+            double C1 = Math.Sqrt(Math.Pow(P5[0], 2) + Math.Pow(P5[1], 2));
+            double D1 = d[1] / C1;
+
+            joints[0] = Math.Round((Math.Atan2(P5[1], P5[0]) - Math.Atan2(D1, Math.Sqrt(1 - Math.Pow(D1, 2)))) * 180 / Math.PI);
+
+            double C2 = P5[2] - d[0];
+            double C3 = Math.Sqrt(Math.Pow(C1, 2) + Math.Pow(C2, 2));
+            double C4 = Math.Sqrt(Math.Pow(r[2], 2) + Math.Pow(d[3], 2));
+
+            double D2 = (Math.Pow(C3, 2) + Math.Pow(r[1], 2) - Math.Pow(C4, 2)) / (2 * r[1] * C3);
+            double D3 = (Math.Pow(C4, 2) + Math.Pow(r[1], 2) - Math.Pow(C3, 2)) / (2 * r[1] * C4);
+
+            double a_1 = Math.Atan2(D1, Math.Sqrt(Math.Abs(1 - Math.Pow(D1, 2))));
+            double a_2 = Math.Atan2(Math.Sqrt(Math.Abs(1 - Math.Pow(D2, 2))), D2);
+
+            double b = Math.Atan2(Math.Sqrt(Math.Abs(1 - Math.Pow(D3, 2))), D3);
+
+            double p_1 = Math.Atan2(P5[1], P5[0]);
+            double p_2 = Math.Atan2(C2, C1);
+
+            joints[1] = Math.Round((a_2 - p_2) * 180 / Math.PI);
+            joints[2] = Math.Round((Math.Atan2(Math.Sqrt(1 - Math.Pow(D3, 2)), D3) * 180 / Math.PI) - 90);
+
+
+
+            double[,] T0_3 = { { 1, 0, 0 ,0 },
+                               { 0, 1, 0, 0 },
+                               { 0, 0, 1, 0 },
+                               { 0, 0, 0, 1 } };
+
+            T0_3 = Matrix.axisToAxis(T0_3, joints[0], d[0], r[0], alpha[0]);
+            T0_3 = Matrix.axisToAxis(T0_3, joints[1], d[1], r[1], alpha[1]);
+            T0_3 = Matrix.axisToAxis(T0_3, joints[2], d[2], r[2], alpha[2]);
+
+            Matrix.print(T0_3);
+            Console.WriteLine();
+
+            double[,] T0_3_inv = Matrix.getInverseMatrix(T0_3);
+
+            Matrix.print(T0_3_inv);
+            Console.WriteLine();
+
+
+            double[,] T3_6 = Matrix.multiplyMatrix(T0_3_inv, calc3);
+
+            Matrix.print(T3_6);
+            Console.WriteLine();
+
+            joints[3] = Math.Round((Math.Atan2(T3_6[1, 2], T3_6[0, 2])) * 180 / Math.PI);
+            joints[4] = Math.Round((Math.Atan2(Math.Sqrt(Math.Abs(1 - Math.Pow(T3_6[2, 2], 2))), T3_6[2, 2])) * 180 / Math.PI);
+            joints[5] = Math.Round((Math.Atan2(T3_6[2, 1], -T3_6[2, 0])) * 180 / Math.PI);
+
+            Console.WriteLine("Calculated joints: ");
+
+            foreach (double j in joints)
+            {
+                Console.WriteLine(j);
+            }
+
+            Console.ReadLine();
+
+
 
         }
 
